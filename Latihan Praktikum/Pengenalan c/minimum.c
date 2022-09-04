1c9c0f23dd9f9b0ebb7a50a772cd67accfee58e8
Jazmy Izzati Alamsyah <88818677+Mipol2@users.noreply.github.com>
2022-09-04 20:04:27 +0700

    menambahkan minimum.c

---
+++ Latihan Praktikum/Pengenalan c/minimum.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c, min;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a<b){
+        if (a<c){
+            min = a;
+        }
+        else{
+            min = c;
+        }
+    }
+    else{
+        if (b<c){
+            min = b;
+        }
+        else{
+            min = c;
+        }
+    }
+
+    printf("%d\n", min);
+    return 0;
+}
